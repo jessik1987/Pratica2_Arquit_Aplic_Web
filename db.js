dd18cb3a90f660e5c01fb661d681b37a85685348
Jéssica Caroline <79118592+jessik1987@users.noreply.github.com>
2022-03-31 21:41:57 -0300

    Update db.js

--- db.js
+++ db.js
@@ -4,26 +4,27 @@ mongoClient.connect("mongodb://localhost/workshoptdc")
 .then(conn => global.conn = conn.db("workshoptdc"))
 .catch(err => console.log(err))
 
-function findAll(callback){
-    global.conn.collection("filmes").find
-    ({}).toArray(callback);
+function findAll(){
+    global.conn.collection("filmes").find().toArray();
     }
 
-function insert(customer, callback){
+function insert(customer){
      global.conn.collection("filmes").insertOne(customer);
      }    
+
+const ObjectId  = require("workshoptdc").ObjectId;
 
 function findOne(id) {
     return global.conn.collection("filmes").findOne(new ObjectId(id));
         }
 
-function update(id, customer, callback){
-     global.conn.collection("filmes").updateOne({_id:new ObjectId(id)}, customer, callback);
+function update(id, customer){
+     global.conn.collection("filmes").updateOne({_id:new ObjectId(id)}, customer);
 }
 
 
-function deleteOne(id, callback){
-    global.conn.collection("filmes").deleteOne({_id: new ObjectId(id)}, callback);
+function deleteOne(id){
+    global.conn.collection("filmes").deleteOne({_id: new ObjectId(id)});
     }
     module.exports = { findAll, insert, findOne, update, deleteOne }
 
